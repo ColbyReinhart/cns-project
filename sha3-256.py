09a2f3996746551101c80e7aea108903fb8808e3
ColbyReinhart <colbyreinhart@gmail.com>
2022-04-21 16:32:58 -0400

    Started from scratch. Implemented stringToBits, bitsToStateArray

--- sha3-256.py
+++ sha3-256.py
@@ -3,46 +3,35 @@
 # CS-45203: Computer Network Security
 # 4-14-2022
 
-from cmath import log
+import sys
+import numpy
+import argparse
 
-# GLOBAL CONSTANTS
-b = 256 # size of message digest/state array
-w = b / 25
-l = log(w, 2)
+# GLOBAL HARD-CODED CONSTANTS FOR KECCAK
+l = 6		# Change this as desired from {0,1,2,3,4,5,6}
+w = 2 ** l	# The width of the z-index of the state array
+b = 25 * w	# 5 * 5 * w, the width of the entire state array
 
-# STEP MAPPING SPECIFICATIONS
-# The five step mappings that comprise a round are θ, ρ, π, χ, ι
+# Convert in input character string into bits
+def stringToBits(input):
+	result = ''
+	for char in input:
+		letterCode = ord(char)				# get the letter code
+		byte = '{0:08b}'.format(letterCode)	# convert to binary
+		byte = byte[::-1]					# reverse, convert to little endian
+		result += byte						# add byte to the result
+	result += '01100000' # Denote SHA3 with appropriate suffix
 
-# Specification of Algorithm 1: θ(A)
-# Input: state array A, which is a 3-dimensional array
-# Output: state array A'
-def theta(A):
-	# For all pairs (x,z) such that 0 <= x <= 5 and 0 <= z <= w, let
-	# C[x,z]=A[x,0,z] ⊕ A[x,1,z] ⊕ A[x,2,z] ⊕ A[x,3,z] ⊕ A[x,4,z]
-	C = []
+# Convert a bitstring into a 5*5*w state array
+def bitsToStateArray(input):
+	result = []
 	for x in range(5):
-		C.append([])
-		for z in range(w):
-			result = A[x,0,z] ^ A[x,1,z] ^ A[x,2,z] ^ A[x,3,z] ^ A[x,4,z]
-			C[x].append(result)
-			
-	# For all pairs (x, z) such that 0≤x<5 and 0≤z<w let 
-	# D[x,z]=C[(x - 1) mod 5, z] ⊕ C[(x + 1) mod 5, (z – 1) mod w]
-	D = []
-	for x in range(5):
-		D.append([])
-		for z in range(w):
-			result = C[(x - 1) % 5, z] ^ C[(x + 1) % 5, (z - 1) % w]
-			D[x].append(result)
-			
-	# For all triples (x, y, z) such that 0≤x<5, 0≤y<5, and 0≤z<w, let
-	# A′[x, y,z] = A[x, y,z] ⊕ D[x,z]
-	Aprime = []
-	for x in range(5):
-		Aprime.append([])
+		result.append([])
 		for y in range(5):
-			Aprime[x].append([])
+			result.append([])
 			for z in range(w):
-				Aprime[x][y].append(A[x][y][z] ^ D[x][z])
-	
-	return Aprime
+				value = (w * ((5 * y) + x) + z)				# generate value
+				if (value < b):								# Index the string
+					result[x][y].append(int(input[value]))	# Fill in the state
+	return result
+
