955ec7d160c1b3ac93176433b87861e19d1193d5
Tyler <tgargas2@kent.edu>
2022-05-10 21:06:59 -0400

    bruh

--- test_collision.py
+++ test_collision.py
@@ -10,7 +10,9 @@
 import hashlib                              # Library containing another implementation of SHA3-256
 from random import choice, randint          # Used to randomly select characters and numbers 
 from string import ascii_letters,  digits   # ASCII character (lower and upper), and digits 0-9
-import timeit                               #Library for timing program runtime
+import timeit                               # Library for timing program runtime
+
+from sha3 import *                          # Importing our implementation
 
 
 # The logic behind our collision testing is simple: the dictionary data type in python
@@ -41,7 +43,39 @@ for t in range(testSize) :
     #     print("\tTest", t)
     #     # print("\tInput:", input)
     #     print("\tHash:", hash)
-    testOutput[index] = hash
+    testOutput[hash] = index
+    index += 1
+
+print("\n\tNumber of Inputs:\t", testSize)
+print("\tNumber of Outputs:\t", len(testOutput), '\n')
+
+if testSize == len(testOutput) :
+    print("\tNo Collision, Testing Successful!")
+else : 
+    print("\tCollisions Detected, Testing Failed!")
+
+end = timeit.default_timer()
+
+print("\tTime:", end - start, "seconds")
+
+print("\nDone testing Library Implementation")
+
+
+# Testing our SHA3-256 implementation
+print("\nBegin Testing Our Implementation\n")
+start = timeit.default_timer()
+
+index = 0
+testOutput = {}
+for t in range(testSize) :
+    inputSize = randint(1000, 100000)
+    input = get_random_string(inputSize)
+    hash = hashlib.sha256(input.encode('utf-8')).hexdigest()
+    # if t % 100 == 0:
+    #     print("\tTest", t)
+    #     # print("\tInput:", input)
+    #     print("\tHash:", hash)
+    testOutput[hash] = index
     index += 1
 
 print("\n\tNumber of Inputs:\t", testSize)
